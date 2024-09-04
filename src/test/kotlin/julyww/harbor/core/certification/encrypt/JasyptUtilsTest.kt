b700ffb49b04ca98067e1216a3bfda31f10683be
Jiaqi.Chen <julycw@163.com>
2024-09-04 16:09:32 +0800

    fix

---
+++ src/test/kotlin/julyww/harbor/core/certification/encrypt/JasyptUtilsTest.kt
@@ -0,0 +1,15 @@
+package julyww.harbor.core.certification.encrypt
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class JasyptUtilsTest {
+    @Test
+    fun testJasyptUtils() {
+        val password = "harbor"
+        val v = "hello"
+        val encrypt = JasyptUtils.encrypt(v, password)
+        val decrypt = JasyptUtils.decrypt(encrypt, password)
+        Assertions.assertEquals(v, decrypt)
+    }
+}
