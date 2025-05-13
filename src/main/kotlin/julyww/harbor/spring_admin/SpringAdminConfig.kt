0e8228229f59900acbde74c604eafe7dc0ae2f16
陈佳琪 <jiaqi.chen@trustway.cn>
2025-05-13 10:43:12 +0800

    修复异常offline

--- src/main/kotlin/julyww/harbor/spring_admin/SpringAdminConfig.kt
+++ src/main/kotlin/julyww/harbor/spring_admin/SpringAdminConfig.kt
@@ -2,6 +2,8 @@ package julyww.harbor.spring_admin
 
 import cn.trustway.nb.common.auth.autoconfig.remote.AuthServiceProvider
 import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import com.google.common.cache.LoadingCache
 import de.codecentric.boot.admin.server.domain.entities.Instance
 import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider
 import de.codecentric.boot.admin.server.web.client.InstanceExchangeFilterFunction
@@ -9,19 +11,31 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import java.time.Duration
+import java.util.concurrent.Executors
 
 
 @Configuration
 class SpringAdminConfig {
 
-    private val cache = CacheBuilder.newBuilder()
-        .expireAfterWrite(Duration.ofMinutes(1))
-        .build<String, String>()
-
     @Bean
     fun customHttpHeadersProvider(
         authServiceProvider: AuthServiceProvider
     ): HttpHeadersProvider {
+        val cache: LoadingCache<String, String> = CacheBuilder
+            .newBuilder()
+            .expireAfterWrite(Duration.ofMinutes(10))
+            .refreshAfterWrite(Duration.ofMinutes(5))
+            .build(
+                CacheLoader.asyncReloading(
+                    object : CacheLoader<String, String>() {
+                        override fun load(key: String): String {
+                            return authServiceProvider.getJwtToken("system", Duration.ofMinutes(10))
+                        }
+                    },
+                    Executors.newFixedThreadPool(1)
+                )
+            )
+
         return HttpHeadersProvider { instance: Instance? ->
             val token = cache.get("token") {
                 authServiceProvider.getJwtToken("system", Duration.ofMinutes(2))
